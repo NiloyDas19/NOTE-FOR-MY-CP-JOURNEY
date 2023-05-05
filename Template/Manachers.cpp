9923b8a2efb3a5c7b27b1fb492210071580ce200
Niloy Das <niloydas5215@gmail.com>
2023-05-05 19:16:31 +0600

    Update Manachers.cpp

--- Template/Manachers.cpp
+++ Template/Manachers.cpp
@@ -9,3 +9,64 @@ vector<int> manacher(string &s) {
 			l[i + k] = min(l[i - k], j - k);
 	} return l;
 }
+
+
+
+// For finding the longest palindromic substring
+
+#define SIZE 1000005
+
+int P[SIZE * 2];
+
+// Transform S into new string with special characters inserted.
+string convertToNewString(const string &s) {
+    string newString = "@";
+
+    for (int i = 0; i < s.size(); i++) {
+        newString += "#" + s.substr(i, 1);
+    }
+
+    newString += "#$";
+    return newString;
+}
+
+string longestPalindromeSubstring(const string &s) {
+    string Q = convertToNewString(s);
+    int c = 0, r = 0;                // current center, right limit
+
+    for (int i = 1; i < Q.size() - 1; i++) {
+        // find the corresponding letter in the palidrome subString
+        int iMirror = c - (i - c);
+
+        if(r > i) {
+            P[i] = min(r - i, P[iMirror]);
+        }
+
+        // expanding around center i
+        while (Q[i + 1 + P[i]] == Q[i - 1 - P[i]]){
+            P[i]++;
+        }
+
+        // Update c,r in case if the palindrome centered at i expands past r,
+        if (i + P[i] > r) {
+            c = i;              // next center = i
+            r = i + P[i];
+        }
+    }
+
+    // Find the longest palindrome length in p.
+
+    int maxPalindrome = 0;
+    int centerIndex = 0;
+
+    for (int i = 1; i < Q.size() - 1; i++) {
+
+        if (P[i] > maxPalindrome) {
+            maxPalindrome = P[i];
+            centerIndex = i;
+        }
+    }
+
+    // cout << maxPalindrome << "\n";
+    return s.substr( (centerIndex - 1 - maxPalindrome) / 2, maxPalindrome);
+}
